c9e7a52ef18d1b98c48ba2cab1ed19f3bde356e6
Juan Sebastian <jsfc2199@gmail.com>
2022-09-02 11:35:49 -0500

    Proyecto inicializado con cada componente y su servicio para la API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RickMortyComponent } from './components/rick-morty/rick-morty.component';
+import { MainComponent } from './components/principal/main/main.component';
+import { HeaderComponent } from './components/principal/header/header.component';
+import { MenuComponent } from './components/principal/menu/menu.component';
+import { LayoutComponent } from './components/principal/layout/layout.component';
+import { FooterComponent } from './components/principal/footer/footer.component';
+import { PersonajesComponent } from './components/personajes/personajes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RickMortyComponent,
+    MainComponent,
+    HeaderComponent,
+    MenuComponent,
+    LayoutComponent,
+    FooterComponent,
+    PersonajesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/rick-morty/rick-morty.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rick-morty',
+  templateUrl: './rick-morty.component.html',
+  styleUrls: ['./rick-morty.component.css']
+})
+export class RickMortyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
